2362a602cdc75779cf68c581f0541654d6b91b9b
Toby Morgan <tobyjmorgan@yahoo.com>
2016-11-20 15:04:02 -0500

    Initial Commit

---
+++ AmusementParkPart2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmusementParkPart2
+//
+//  Created by redBred LLC on 11/20/16.
+//  Copyright © 2016 redBred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
